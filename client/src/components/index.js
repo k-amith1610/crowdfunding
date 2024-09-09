b945616a70c18e9e359bf003243b5f1b4415f508
k-amith1610 <2110030372@klh.edu.in>
2024-09-09 16:59:10 +0530

    Done with showing fundcards in Homepage

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,6 @@ export { default as Sidebar } from "./Sidebar";
 export { default as Navbar } from "./Navbar";
 export { default as CustomButton } from "./CustomButton";
 export { default as FormField } from "./FormField";
-export { default as Loader } from "./Loader"
+export { default as Loader } from "./Loader";
+export { default as DisplayCampaigns } from "./DisplayCampaigns";
+export { default as FundCard } from "./FundCard";
--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -31,6 +31,23 @@ export const StateContextProvider = ({ children }) => {
     }
 
 
+    const getCampaigns = async () => {
+        const campaigns = await contract.call('getCampaigns');
+        // console.log(campaigns);
+        const parsedCampaigns = campaigns.map((campaign, i) => ({
+            owner: campaign.owner,
+            title: campaign.title,
+            description: campaign.description,
+            target: ethers.utils.formatEther(campaign.target.toString()),
+            deadline: campaign.deadline.toNumber(),
+            amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+            image: campaign.image,
+            pId: i
+        }))
+        // console.log(parsedCampaigns);
+        return parsedCampaigns
+    }
+
     return (
         <StateContext.Provider
             value={{
@@ -38,6 +55,7 @@ export const StateContextProvider = ({ children }) => {
                 contract,
                 connect,
                 createCampaign: publishCampaign,
+                getCampaigns,
             }}
         >
             {children}
